789840a2db32931d678696927b4e33e4afd3814e
kolamala <mohan.kolamala@gmail.com>
2021-04-25 19:38:50 -0700

    added content type as json to Http Response

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -9,4 +9,7 @@ def index(request):
         "Message":'The learning materials and instructions are good'
     }
  
-    return HttpResponse(json.dumps(data))
+    return HttpResponse(json.dumps(data),headers={
+        'Content-Type':'application/json',
+        'Content-Description':'About myself and my track info'
+    })
