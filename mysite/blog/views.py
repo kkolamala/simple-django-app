fa071f3dda91e1eb6eee5c4e8d707ec966014371
kolamala <mohan.kolamala@gmail.com>
2021-04-25 20:27:00 -0700

    updated to JsonResponse from HttpResponse

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
-import json
+from django.http import JsonResponse
+#import json
 # Create your views here.
 
 def index(request):
@@ -8,8 +8,10 @@ def index(request):
         "Track":'Backend Python Track',
         "Message":'The learning materials and instructions are good'
     }
+    
+    return JsonResponse(data)
  
-    return HttpResponse(json.dumps(data),headers={
-        'Content-Type':'application/json',
-        'Content-Description':'About myself and my track info'
-    })
+    # return HttpResponse(json.dumps(data),headers={
+    #     'Content-Type':'application/json',
+    #     'Content-Description':'About myself and my track info'
+    # })
